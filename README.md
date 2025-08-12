6bd6a64a114324450560618faaf8ad9259943288
Franciele <francieleaquino.m@outlook.com>
2025-08-12 17:50:29 +0100

    commit inicial com a base estrutural do projeto

---
+++ README.md
@@ -0,0 +1,40 @@
+Projeto 7: Around The EU
+Around The EU é o sétimo projeto do curso de Desenvolvimento Web da TripleTen.
+Uma página web interativa que exibe uma galeria de fotos de lugares visitados pela Europa.
+
+O utilizador pode curtir imagens e, futuramente, adicionar ou remover fotos da galeria.
+
+🚀 Tecnologias Utilizadas
+HTML
+
+CSS
+
+@font-face para fontes personalizadas
+
+Layouts responsivos
+
+JavaScript
+
+Manipulação do DOM
+
+Funcionalidades atuais:
+
+Abrir/fechar modal de edição de perfil
+
+Atualizar nome e descrição do perfil em tempo real
+
+Fechar modal com Esc ou clique fora da área
+
+Figma
+
+Baseado em layouts criados no Figma
+
+## Como Usar
+
+1. Clone este repositório:
+   ```bash
+   git clone https://github.com/Franciele-M-Aquino/web_project_homeland
+   ```
+2. Link do projeto GH pages:
+
+https://franciele-m-aquino.github.io/web_project_homeland/
---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+//Variável//
+
+const modal = document.querySelector(".modal");
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const modalCloseButton = document.querySelector(".modal__close-button");
+
+const profileFormElement = document.querySelector(".modal__form");
+
+const nameInput = profileFormElement.querySelector(".modal__input-name");
+
+const descriptionInput = profileFormElement.querySelector(
+  ".modal__input-description"
+);
+
+//Mudar nome do perfil
+const profileUser = document.querySelector(".profile__user");
+//Mudar nome da descrição
+const profileBio = document.querySelector(".profile__bio");
+
+//Função//
+function openModal() {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal() {
+  modal.classList.remove("modal_is-opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); //impedir que a página seja recarregada toda vez que submeter dados//
+  profileUser.textContent = nameInput.value;
+  profileBio.textContent = descriptionInput.value;
+}
+
+//Evento//
+
+profileEditButton.addEventListener("click", openModal);
+
+modalCloseButton.addEventListener("click", closeModal);
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
